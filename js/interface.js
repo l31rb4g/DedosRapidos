34c588bdeec87e815eb9635c28deddbfa5ec0588
Gabriel Paladino <l31rb4g@gmail.com>
2016-10-25 13:50:07 -0200

    interface scenario

--- js/interface.js
+++ js/interface.js
@@ -3,6 +3,13 @@ Interface = new Class({
     initialize: function(){
         this.playerLives();
         this.createScore();
+        this.createScenario();
+    },
+    
+    createScenario: function(){
+        new Element('div', {'class': 'board'}).adopt(
+            new Element('div', {'class': 'container'})
+        ).inject($$('body'[0]));
     },
 
     playerLives: function(){

5b61987fbc83d3b5c3d4beb5efedc4adb70836f1
rogerio <rogerio@rogeriocustodio.com.br>
2016-10-25 18:54:56 -0200

    Ajustes

--- js/lightbox.js
+++ js/lightbox.js
@@ -4,73 +4,54 @@ LightBox = new Class({
     },
 
     addEvents: function(){
-        this.overlay = new Element('div', {
+        overlay = new Element('div', {
             'class': 'overlay'
         }).inject($$('body')[0]);
 
-        this.overlay.tween("opacity", "0.8");
+        overlay.tween("opacity", "0.8");
 
-        this.box = new Element('div', {
+        box = new Element('div', {
             'class': 'box'
         }).adopt(
-            new Element('form', {
-                'method': 'post',
-                'events': {
-                    'submit': function(ev){
-                        ev.stop();
-                        var nickname = $('nickName').get('value');
-                        if (nickname){
-                            new Request({
-                                'method': 'post',
-                                'url': '/api.php?action=score',
-                                'data': {
-                                    'score': Game.score.get('text'),
-                                    'nickname': nickname
-                                }
-                            }).send();
-                            this.close();
-                        }
-                    }.bind(this)
-                }
+            new Element('div', {
+                'class': 'formItem'
             }).adopt(
-                new Element('div', {
-                    'class': 'formItem'
+                new Element('label', {
+                    'text': 'Nickname: '
+                }),
+
+                new Element('input', {
+                    'name': 'nickName',
+                    'id': 'nickName',
+                    'type': 'text'
+                }),
+
+                new Element('input', {
+                    'id': 'submit',
+                    'type': 'submit',
+                    'value': 'Send',
+                    'events': {
+                        click: function(){
+                            if($("nickName").get('value').trim() != ""){
+                                location.reload();
+                            }else{
+                                $("nickName").addClass('error');
+                            }
+                        }.bind(this)
+                    }
+                }),
+
+                new Element('p', {
+                    'class': 'pontuacao',
+                    'text': 'Score: '
                 }).adopt(
-                    new Element('label', {
-                        'html': 'Nickname: &nbsp;'
-                    }),
-
-                    new Element('input', {
-                        'name': 'nickName',
-                        'id': 'nickName',
-                        'type': 'text',
-                        'required': 'required',
-                        'autofocus': 'autofocus'
-                    }),
-
-                    new Element('input', {
-                        'id': 'submit',
-                        'type': 'submit',
-                        'value': 'Send'
-                    }),
-
-                    new Element('p', {
-                        'class': 'pontuacao',
-                        'text': 'Score: '
-                    }).adopt(
-                        new Element('span', {
-                            'text': '500'
-                        })
-                    )
+                    new Element('span', {
+                        'text': '500'
+                    })
                 )
             )
         ).inject($$('body')[0]);
 
-        this.box.tween("opacity", "1");
-    },
-    
-    close: function(){
-        this.overlay.dispose();
-        this.box.dispose();
+        box.tween("opacity", "1");
     }
 });
